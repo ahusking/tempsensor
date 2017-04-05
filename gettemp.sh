66f5cbaa490bfb1befed4b62ef3074e05bddda82
root <andrew@husking.id.au>
2017-04-05 19:58:04 +1000

    more loggy's

--- gettemp.sh
+++ gettemp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 Temp=$(pcsensor -c | awk '{print $4}' | cut -c 1-5 )
-echo ${Temp}
+echo "home.iot.beertemp1 ${Temp} `date +%s`"
 echo "home.iot.beertemp1 ${Temp} `date +%s`" | nc -w 1 192.168.1.112 2003
