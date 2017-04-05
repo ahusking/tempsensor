18bd2c63a033457085510981bfd7fe182aa4391b
root <andrew@husking.id.au>
2017-04-05 19:53:51 +1000

    minor updates for running

--- gettemp.sh
+++ gettemp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 Temp=$(pcsensor -c | awk '{print $4}' | cut -c 1-5 )
-#echo ${Temp}
-
-
-echo "beertemp:${Temp}" | nc -w 1 -u home.husking.id.au 8125
+echo ${Temp}
+echo "home.iot.beertemp1 ${Temp} `date +%s`" | nc -w 1 192.168.1.112 2003
--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade
-apt-get install build-essential screen wget curl
+apt-get install build-essential screen wget curl libusb-dev
 cd pcsensor-1.0.1
 make
 cp pcsensor /usr/bin
-
+mkdir /opt/scripts
+cp gettemp.sh /opt/scripts
+(crontab -l 2>/dev/null; echo "*/5 * * * * /bin/bash /opt/scripts/gettemp.sh >> /var/log/gettemp.log") | crontab -
